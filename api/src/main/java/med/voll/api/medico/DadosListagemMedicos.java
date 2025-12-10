dff833a2103fb0ab8fb9793a405611f9da695c86
ThiagoMelo22 <thiagomelo329@hotmail.com>
2025-12-10 17:16:39 -0300

    Added Doctor Update Method

--- api/src/main/java/med/voll/api/medico/DadosListagemMedicos.java
+++ api/src/main/java/med/voll/api/medico/DadosListagemMedicos.java
@@ -1,12 +1,13 @@
 package med.voll.api.medico;
 
 public record DadosListagemMedicos(
+        Long id,
         String nome,
         String email,
         String crm,
         Especialidade escialidade
 ) {
     public DadosListagemMedicos(Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
